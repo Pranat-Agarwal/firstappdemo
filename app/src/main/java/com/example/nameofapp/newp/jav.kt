515f96b72bc02117ca5db112c4d70a3b2fbdf516
Pranat <pranatagarwal100@gmail.com>
2024-06-07 11:34:18 +0530

    new update

---
+++ app/src/main/java/com/example/nameofapp/newp/jav.kt
@@ -0,0 +1,13 @@
+package com.example.nameofapp.newp
+
+class jav(name: String?, age: Int, last: String?) {
+    var name: String? = null
+    var age: Int = 0
+    var last: String? = null
+
+    init {
+        this.name = name
+        this.age = age
+        this.last = last
+    }
+}
---
+++ app/src/main/java/com/example/nameofapp/newp/kot.kt
@@ -0,0 +1,6 @@
+package com.example.nameofapp.newp
+
+class kot {
+    var j:jav=jav("p",1,"l")
+
+}
